51e71f41151ef43ae41f4245098e386b7fd0d932
Imu <48524793+LimHyunJune@users.noreply.github.com>
2023-12-21 22:56:21 +0900

    feat: initial datafetcher

---
+++ datafetcher/src/main/java/com/example/datafetcher/DatafetcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.datafetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatafetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatafetcherApplication.class, args);
+	}
+
+}
