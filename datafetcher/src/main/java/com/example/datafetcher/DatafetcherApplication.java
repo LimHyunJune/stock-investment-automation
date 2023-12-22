18fd7a5e2595d99bdab964a73109eb8bc584fe59
Imu <48524793+LimHyunJune@users.noreply.github.com>
2023-12-22 17:56:49 +0900

    fix: service discovery

--- datafetcher/src/main/java/com/example/datafetcher/DatafetcherApplication.java
+++ datafetcher/src/main/java/com/example/datafetcher/DatafetcherApplication.java
@@ -2,8 +2,12 @@ package com.example.datafetcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = "com.example")
 public class DatafetcherApplication {
 
 	public static void main(String[] args) {
