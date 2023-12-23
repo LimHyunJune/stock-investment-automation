eee4f0d270d211cec559e60e916817dcf97fe8c1
Imu <48524793+LimHyunJune@users.noreply.github.com>
2023-12-23 13:49:10 +0900

    feat: kafka single-broker/topic->data producer, consumer client

--- datafetcher/src/main/java/com/example/controller/DataFetcherController.java
+++ datafetcher/src/main/java/com/example/controller/DataFetcherController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import com.example.kafka.KafkaProducer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/data")
 public class DataFetcherController {
 
+    private final KafkaProducer kafkaProducer;
+    DataFetcherController(KafkaProducer kafkaProducer)
+    {
+        this.kafkaProducer = kafkaProducer;
+    }
     @GetMapping("welcome")
     public String test()
     {
         return "test";
     }
+
 }
+
+
