079159e23e74977fba7759ba07cf4f25fbb9acb4
Imu <48524793+LimHyunJune@users.noreply.github.com>
2023-12-29 23:03:23 +0900

    feat: buy checklist (#1,#2,#5)

--- buy/src/main/java/com/example/robot/BuyChecklist1.java
+++ buy/src/main/java/com/example/robot/BuyChecklist1.java
@@ -3,11 +3,15 @@ package com.example.robot;
 import com.example.dto.StockData;
 import lombok.Data;
 
+import java.util.OptionalDouble;
+
 @Data
 public class BuyChecklist1 implements BuyChecklist {
     public String context = "매년 매출이 증가하는가?";
     @Override
     public boolean run(StockData stockData) {
-        return false;
+        double average = stockData.getRevenueGrowth().stream()
+                .mapToDouble(a->a).average().orElse(0);
+        return average > 0;
     }
 }
--- buy/src/main/java/com/example/robot/BuyChecklist2.java
+++ buy/src/main/java/com/example/robot/BuyChecklist2.java
@@ -6,8 +6,11 @@ import lombok.Data;
 @Data
 public class BuyChecklist2 implements BuyChecklist {
     public String context = "매년 이익이 증가하는가?";
+    private double threshold = 5;
     @Override
     public boolean run(StockData stockData) {
-        return false;
+        double average = stockData.getGrossProfitGrowth().stream()
+                .mapToDouble(a->a).average().orElse(0);
+        return average > 0;
     }
 }
